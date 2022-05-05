aeef2fb46b8a08e98c957c35ed685d0cc6de5dff
Susmitha Dannana <84056805+SusmithA-14@users.noreply.github.com>
2022-05-05 21:48:03 +0530

    Add files via upload

---
+++ graph_dfs.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void DFS(int node,vector<int>&vis,vector<int>adj[])
+{
+    cout<<node<<" ";
+    vis[node]=1;
+    for(auto it:adj[node])
+    {
+        if(!vis[it])
+        {
+            DFS(it,vis,adj);
+        }
+    }
+}
+int main()
+{
+    int n,e;//nodes,edges
+    cin>>n>>e;
+    vector<int>adj[n+1];//adj list
+    int u,v;
+    //adjacency list;
+    for(int i=0;i<e;i++)
+    {
+       cin>>u>>v;//input for every node and edges
+       adj[u].push_back(v);
+       adj[v].push_back(u);
+    }
+    vector<int>vis(n+1,0);
+    DFS(1,vis,adj);
+}
